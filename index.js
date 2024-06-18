9cc1b175b8e6dbf33c4397395274c8f68d1b8c53
rusgyn <rusgynmagsanay@gmail.com>
2024-06-18 16:02:01 -0400

    Modify the temporary IP and place a comment on it.

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Returned IP:' , ip);
 });
 
-fetchCoordsByIP('142.189.254.239', (error, data) => {
+// The code below is temporary and can be commented out.
+// The xxx.xxx.xxx.xxx is a sample location IP address.
+fetchCoordsByIP('xxx.xxx.xxx.xxx', (error, data) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
