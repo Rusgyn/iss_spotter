d8326495518971755f956b7a6d71709973255b3d
rusgyn <rusgynmagsanay@gmail.com>
2024-06-18 15:50:15 -0400

    Add fetchCoordsByIP callback for testing purpose

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -9,3 +9,13 @@ fetchMyIP((error, ip) => {
 
   console.log('It worked! Returned IP:' , ip);
 });
+
+fetchCoordsByIP('142.189.254.239', (error, data) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , data);
+  return;
+})
