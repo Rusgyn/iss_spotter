c3aa2bf6ecbb9f2f81098b0698cd16da97215dc8
rusgyn <rusgynmagsanay@gmail.com>
2024-06-18 16:29:59 -0400

    Implement function that test the function that fetch ISS fly over times

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -11,13 +11,28 @@ fetchMyIP((error, ip) => {
 });
 
 // The code below is temporary and can be commented out.
-// The xxx.xxx.xxx.xxx is a sample location IP address.
-fetchCoordsByIP('xxx.xxx.xxx.xxx', (error, data) => {
+// The xxx.xxx.xxx.xxx is your location IP address. You can use http://ipwho.is/ to get your IP address.
+fetchCoordsByIP('xxx.xxx.xxx.xxx', (error, coordinates) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , data);
+  console.log('It worked! Returned IP:' , coordinates);
   return;
-})
+});
+
+//The xxx stands your location coordinates. You can use http://ipwho.is/ to get your latitude and longitude.
+const coords = {
+  latitude: 'xxx',
+  longitude: 'xxx'
+};
+fetchISSFlyOverTimes(coords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , passTimes);
+  return;
+});
