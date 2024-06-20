37e682805580eef74738f29674dd70f861c59878
rusgyn <rusgynmagsanay@gmail.com>
2024-06-20 17:00:39 -0400

    Implement a function that chains the 3 functions calls to use and returns the final result

--- iss_promised.js
+++ iss_promised.js
@@ -19,22 +19,22 @@ const fetchMyIP = function() {
     });
 };
 
-/* 
+/*
  * Makes a request to ipwho.is using the provided IP address to get its geographical information (latitude/longitude)
  * Input: IP address as a string
  * Returns: Promise of request for lat/lon
  */
 const fetchCoordsByIP = function(ip) {
-    // using promises on needle method to do the request.
-    return needle('get', `http://ipwho.is/${ip}`)
+  // using promises on needle method to do the request.
+  return needle('get', `http://ipwho.is/${ip}`)
     .then((response) => {
       const body = response.body;//retrieve the body value from the response object.
-
+      //retrieves the latitude and longitude from body.
       const coordinates = {
         latitude:body.latitude,
         longitude:body.longitude
-      }
-      
+      };
+        
       return coordinates;
     })
     .catch((error) => {
@@ -42,8 +42,34 @@ const fetchCoordsByIP = function(ip) {
     });
 };
 
+/*
+ * Requests data from https://iss-flyover.herokuapp.com using provided lat/long data
+ * Input: Body containing geo data response from ipwho.is
+ * Returns: Promise of request for fly over data, returned as JSON string
+ */
+
+const fetchISSFlyOverTimes = function(coordinates) {
+
+  return needle('get', `https://iss-flyover.herokuapp.com/json/?lat=${coordinates.latitude}&lon=${coordinates.longitude}`)
+    .then((response) => {
+      const body = response.body;
+      const passTimes = body.response; //// changed the name from response to passTimes for clarification
+      return passTimes;
+    })
+    .catch((error) => {
+      return error;
+    });
+};
+
+const nextISSTimesForMyLocation = function(passTimes) {
+ return fetchMyIP()
+  .then((ip) => fetchCoordsByIP(ip))
+  .then((coordinates) => fetchISSFlyOverTimes(coordinates))
+  .then((passTimes) => {
+    return passTimes;
+  })
+}
 
 module.exports = {
-  fetchMyIP,
-  fetchCoordsByIP
+  nextISSTimesForMyLocation
 };
