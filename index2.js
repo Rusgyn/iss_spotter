65e9bc242ab10ac023a1937136d0bbabe1965d59
rusgyn <rusgynmagsanay@gmail.com>
2024-06-20 17:03:10 -0400

    Implement a function for client to return the next ISS fly times in my location.

--- index2.js
+++ index2.js
@@ -1,5 +1,15 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss_promised');
+const { nextISSTimesForMyLocation} = require('./iss_promised');
 
-fetchMyIP()
-  .then((ip) => fetchCoordsByIP(ip))
-  .then((body) => console.log(body));
+const printPassTimes = function(passTimes) {
+  for (let passTime of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(passTime.risetime);
+    const duration = passTime.duration;
+     console.log(`Next pass at ${dateTime} for ${duration} seconds!`)
+  }
+}
+
+
+nextISSTimesForMyLocation()
+  .then((body) => printPassTimes(body))
+  .catch((error) => console.log(error));
