089ab576cd30f94c151d33d6c07397adceafc928
rusgyn <rusgynmagsanay@gmail.com>
2024-06-20 16:05:08 -0400

    Implement function that retrieves location coordinates as per the generated IP address

--- iss_promised.js
+++ iss_promised.js
@@ -7,7 +7,7 @@ const needle = require('needle');
  */
 
 const fetchMyIP = function() {
-  // using needle and promise method.
+  // using promises on needle method to do the request.
   return needle('get', 'https://api.ipify.org?format=json')
     .then((response) => {
       const body = response.body;//retrieve the body value from the response object. => body {ip: 'xxx.xxx.xxx.xxx'}
@@ -19,4 +19,31 @@ const fetchMyIP = function() {
     });
 };
 
-module.exports = { fetchMyIP };
+/* 
+ * Makes a request to ipwho.is using the provided IP address to get its geographical information (latitude/longitude)
+ * Input: IP address as a string
+ * Returns: Promise of request for lat/lon
+ */
+const fetchCoordsByIP = function(ip) {
+    // using promises on needle method to do the request.
+    return needle('get', `http://ipwho.is/${ip}`)
+    .then((response) => {
+      const body = response.body;//retrieve the body value from the response object.
+
+      const coordinates = {
+        latitude:body.latitude,
+        longitude:body.longitude
+      }
+      
+      return coordinates;
+    })
+    .catch((error) => {
+      return error;
+    });
+};
+
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
